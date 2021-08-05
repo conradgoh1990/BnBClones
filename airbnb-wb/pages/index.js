186e942aec14f4ec322b2229eccc84c852a603ff
conradGoh <conrad.goh.1990@gmail.com>
2021-08-05 22:08:55 +0800

    AirBnB Day 2

---
+++ airbnb-wb/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Banner from '../components/Banner'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>AirBnB</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <Banner />
+      
+
+
+    </div>
+  )
+}
