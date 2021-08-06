d52780c58515a41dd82779b5f71df289bb9ab5e5
conradGoh <conrad.goh.1990@gmail.com>
2021-08-06 22:50:55 +0800

    Airbnb-Clone Day 2

---
+++ airbnb-wb/pages/search.js
@@ -0,0 +1,14 @@
+// import Footer from "../components/Footer";
+// // import Footer from "../components/Header";
+
+function search() {
+    return (
+        <div>
+            {/* <Header /> */}
+            
+            <h1> </h1>
+        </div>
+    )
+}
+
+export default search
